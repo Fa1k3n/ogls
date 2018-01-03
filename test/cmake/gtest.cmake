0f1e18b91ca112368841d7cb4d6596bd625cc9a0
jfalkens <johan.falkenstrom@flir.se>
2018-01-03 09:55:45 +0100

    updates

--- test/cmake/gtest.cmake
+++ test/cmake/gtest.cmake
@@ -1,11 +1,11 @@
 set(GOOGLETEST_ROOT gtest/googletest CACHE STRING "Google Test source root")
-set(GOOGLEMOCK_ROOT gtest/googlemock CACHE STRING "Google Mock source root")
+#set(GOOGLEMOCK_ROOT gtest/googlemock CACHE STRING "Google Mock source root")
 
 include_directories(SYSTEM
     ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLETEST_ROOT}
     ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLETEST_ROOT}/include
-    ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}
-    ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/include
+ #   ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}
+ #   ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/include
     )
 
 set(GOOGLETEST_SOURCES
@@ -13,18 +13,18 @@ set(GOOGLETEST_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
     )
 
-set(GOOGLEMOCK_SOURCES
-    ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/src/gmock-all.cc
-    ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/src/gmock_main.cc
-    )
+#set(GOOGLEMOCK_SOURCES
+#    ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/src/gmock-all.cc
+#    ${CMAKE_CURRENT_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/src/gmock_main.cc
+#    )
 
 foreach(_source ${GOOGLETEST_SOURCES})
     set_source_files_properties(${_source} PROPERTIES GENERATED 1)
 endforeach()
 
-foreach(_source ${GOOGLEMOCK_SOURCES})
-    set_source_files_properties(${_source} PROPERTIES GENERATED 1)
-endforeach()
+#foreach(_source ${GOOGLEMOCK_SOURCES})
+#    set_source_files_properties(${_source} PROPERTIES GENERATED 1)
+#endforeach()
 
 add_library(gtest ${GOOGLETEST_SOURCES})
-add_library(gmock ${GOOGLEMOCK_SOURCES})
+#add_library(gmock ${GOOGLEMOCK_SOURCES})
